e5ffc50bb71fdeacc6b3eb20756724652ea19563
Nino <nino.zumstein@gmail.com>
2018-03-26 17:38:14 +0200

    added styles

--- burgerbuilder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burgerbuilder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react';
 import classes from './BurgerIngredient.css';
+import PropTypes from 'prop-types';
 
-const burgerIngredient = (props) => {
+class BurgerIngredient extends Component {
 
-    let ingredient = null;
+    
+    render() {
+        let ingredient = null;
 
-    switch(props.type) {
+    
+    switch(this.props.type) {
         case ('bread-bottom'): 
             ingredient = <div className={classes.BreadBottom}></div>;
             break;
@@ -34,6 +38,11 @@ const burgerIngredient = (props) => {
     }
 
     return ingredient;
+    }
 }
 
-export default burgerIngredient;
+BurgerIngredient.propTypes = {
+    type: PropTypes.string.isRequired
+}
+
+export default BurgerIngredient;
--- burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
 
     render() {
         return (
             <Auxiliary>
-                <div>
-                    Burger
-                </div>
-                <div>
-                    Build Control
-                </div>
+                <Burger />
             </Auxiliary>
         );
     }
