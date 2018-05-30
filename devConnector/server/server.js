0da3105b6986c12033b1f0c308184fb739b2ffbd
Nino <nino.zumstein@gmail.com>
2018-05-30 21:16:32 +0200

    added basic server setup

---
+++ devConnector/server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+const db = require('./config/keys').mongoURI;
+
+// MongooseDB Connection
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connection established'))
+  .catch((err) => console.log(err));
+
+
+app.get('/', (req, res) => res.send('hello'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
